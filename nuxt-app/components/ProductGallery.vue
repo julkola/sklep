8f1fe5d7ededd07a7c90706a59a2890f7447f3b4
Ju <julia.kolano66@gmail.com>
2023-03-26 03:03:43 +0200

    product gallery & title

---
+++ nuxt-app/components/ProductGallery.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex">
+        <div class="flex flex-col h-full">
+            <img v-for="photo in photos" :src="photo" :alt="photo" class="object-contain w-36 h-36 bg-gray-300 rounded mb-4 last:mb-0">
+        </div>
+        <div class="overflow-hidden w-96 ml-4">
+            <div class="flex h-full max-w-none">
+                <img v-for="photo in photos" :src="photo" :alt="photo" class="flex-none object-contain w-96 bg-gray-300 rounded">
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+    const photos = [
+        'dada',
+        'caca',
+        'baca'
+    ]
+</script>
--- nuxt-app/pages/product.vue
+++ nuxt-app/pages/product.vue
@@ -1,17 +1,19 @@
 <template>
-    <main>
-        <div class="">
-            // gallery
-        </div>
-        <div class="">
-
+    <main class="text-gray-900">
+        <div class="flex">
+            <ProductGallery/>
+            <div class="ml-4">
+                <h1 class="text-lg font-bold">{{ product.name }}</h1>
+                <p>{{ product.desc }}</p>
+            </div>
         </div>
     </main>
 </template>
 <script setup lang="ts">
 const product = {
     id: "1234",
-    name: "krem",
+    name: "Anwen - Spanwen - Kula do Kąpieli o Zapachu Bzu - 100g",
+    desc: "Kula do kąpieli o zapachu bzu",
     price: 28.30,
     available: 12,
     poducerId: "123",
