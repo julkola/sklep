72f19a87817c24417f6c408e046e14e806e9536f
Ju <julia.kolano66@gmail.com>
2023-03-26 01:30:22 +0100

    header starter styling

--- nuxt-app/components/TheHeader.vue
+++ nuxt-app/components/TheHeader.vue
@@ -8,27 +8,35 @@ import UserIcon from './UserIcon.vue';
 import UserIcon from './UserIcon.vue';
 
 <template>
-    <header class="justify-between">
-        <svg class="logo"></svg>
-        <div class="flex">
-            <div class="flex">
-                <input type="text">
-                <searchIcon></searchIcon>
+    <header class="">
+        <div class="flex justify-between py-2 px-4 border-b-2 border-gray-900">
+            <nuxtLogo></nuxtLogo>
+            <div class="flex items-center">
+                <div class="relative h-10">
+                    <input type="text" class="h-10 rounded-full border-gray-900 border-2 pl-4 pr-12">
+                    <button class="absolute top-0 bottom-0 right-0 flex items-center justify-center bg-gray-900 text-white rounded-full w-10 h-10">
+                        <searchIcon></searchIcon>
+                    </button>
+                </div>
+                <button class="flex items-center justify-center bg-gray-900 text-white rounded-full ml-4 w-10 h-10">
+                    <UserIcon></UserIcon>
+                </button>
+                <button class="flex items-center justify-center bg-gray-900 text-white rounded-full ml-4 w-10 h-10">
+                    <HeartIcon></HeartIcon>
+                </button>
+                <button class="flex items-center justify-center bg-gray-900 text-white rounded-full ml-4 px-4 h-10">
+                    <BasketIcon class="mr-2"></BasketIcon>
+                    <span>
+                        {{ cart.sum }}&nbsp;zł
+                    </span>
+                </button>
             </div>
-            <button>
-                <UserIcon></UserIcon>
-            </button>
-            <button>
-                <HeartIcon></HeartIcon>
-            </button>
-            <button>
-                <BasketIcon></BasketIcon>
-                <span>
-                    {{ cart.sum }}&nbsp;zł
-                </span>
-            </button>
         </div>
-
+        <div class="flex py-1 px-4 text-lg font-bold text-gray-900 border-b-2 border-gray-900">
+            <nuxt-link class="px-2 py-1 mr-4 last:mr-0 relative after:absolute after:h-1 after:bg-gray-900 after:rounded-full after:bottom-0 after:right-0 after:left-0 after:origin-left after:duration-150 after:scale-x-0 hover:after:scale-x-100 cursor-pointer">Włosy</nuxt-link>
+            <nuxt-link class="px-2 py-1 mr-4 last:mr-0 relative after:absolute after:h-1 after:bg-gray-900 after:rounded-full after:bottom-0 after:right-0 after:left-0 after:origin-left after:duration-150 after:scale-x-0 hover:after:scale-x-100 cursor-pointer">Ciało</nuxt-link>
+            <nuxt-link class="px-2 py-1 mr-4 last:mr-0 relative after:absolute after:h-1 after:bg-gray-900 after:rounded-full after:bottom-0 after:right-0 after:left-0 after:origin-left after:duration-150 after:scale-x-0 hover:after:scale-x-100 cursor-pointer">Twarz</nuxt-link>
+        </div>
     </header>
 </template>
 <script setup lang="ts">
