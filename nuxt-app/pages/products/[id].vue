43c5d49608ab6bf31bcfc05ed7b1cb29a0a935d0
Ju <julia.kolano66@gmail.com>
2023-03-28 23:21:11 +0200

    dynamic routes + product api

--- nuxt-app/pages/products/[id].vue
+++ nuxt-app/pages/products/[id].vue
@@ -3,15 +3,15 @@
         <div class="flex">
             <ProductGallery/>
             <div class="ml-4">
-                <h1 class="text-lg font-bold">{{ product.name }}</h1>
-                <p>{{ product.desc }}</p>
+                <h1 class="text-lg font-bold">{{ product!.name }}</h1>
+                <p>{{ product!.desc }}</p>
                 <div class="flex flex-wrap py-1">
                     <nuxt-link
-                        v-for="variant in product.variants"
+                        v-for="variant in product!.variants"
                         class="border-gray-900 border-2 rounded-full px-3 text-sm mr-1 last:mr-0 my-1"
-                        :disabled="variant.id === product.id"
+                        :disabled="variant.id === product!.id"
                         :to="variant.id"
-                        :class="variant.id === product.id ? 'bg-gray-900 text-white' : ''"
+                        :class="variant.id === product!.id ? 'bg-gray-900 text-white' : ''"
                     >
                         {{ variant.variantName }}
                     </nuxt-link>
@@ -20,7 +20,7 @@
                     <div class="relative">
                         <button @click="decreaseQuantity" :disabled="addToCartQuantity === 1" class="absolute left-0 w-8 h-8 bg-gray-400 text-white rounded-full">-</button>
                         <input v-model="addToCartQuantity" type="text" inputmode="numeric" class="w-32 h-8 px-12 text-center border-2 border-gray-900 rounded-full">
-                        <button @click="increaseQuantity" :disabled="addToCartQuantity === product.available" class="absolute right-0 w-8 h-8 bg-gray-400 text-white rounded-full">+</button>
+                        <button @click="increaseQuantity" :disabled="addToCartQuantity === product!.available" class="absolute right-0 w-8 h-8 bg-gray-400 text-white rounded-full">+</button>
                     </div>
                     <button @click="addToCart" class="flex-1 bg-gray-900 text-white rounded-full">
                         Dodaj do koszyka
@@ -35,30 +35,7 @@ import { useCartStore } from '~~/stores/cart';
 
 const cart = useCartStore();
 const route = useRoute();
-
-const product = {
-    id: route.params.id as string,
-    name: "Anwen - Spanwen - Kula do Kąpieli o Zapachu Bzu - 100g",
-    desc: "Kula do kąpieli o zapachu bzu",
-    price: 28.30,
-    available: 12,
-    poducerId: "123",
-    producerName: "SkinTra",
-    variants: [
-        {
-            id: "1231",
-            variantName: "Magnolia"
-        },
-        {
-            id: "1232",
-            variantName: "Róża"
-        },
-        {
-            id: "1234",
-            variantName: "Bez"
-        },
-    ]
-}
+const { data: product} = useFetch(`/api/product/${route.params.id}`)
 const productInCart = cart.getProduct(product.id);
 const addToCartQuantity = productInCart ? ref(productInCart.quantity) : ref(1);
 function increaseQuantity () {
---
+++ nuxt-app/server/api/product/[id].ts
@@ -0,0 +1,74 @@
+const products = [
+    {
+        id: "1231",
+        name: "Anwen - Spanwen - Kula do Kąpieli o Zapachu Magnolia - 100g",
+        desc: "Kula do kąpieli o zapachu Magnolia",
+        price: 28.30,
+        available: 12,
+        poducerId: "123",
+        producerName: "SkinTra",
+        variants: [
+            {
+                id: "1231",
+                variantName: "Magnolia"
+            },
+            {
+                id: "1232",
+                variantName: "Róża"
+            },
+            {
+                id: "1234",
+                variantName: "Bez"
+            },
+        ]
+    },
+    {
+        id: "1232",
+        name: "Anwen - Spanwen - Kula do Kąpieli o Zapachu Róża - 100g",
+        desc: "Kula do kąpieli o zapachu Róża",
+        price: 28.30,
+        available: 12,
+        poducerId: "123",
+        producerName: "SkinTra",
+        variants: [
+            {
+                id: "1231",
+                variantName: "Magnolia"
+            },
+            {
+                id: "1232",
+                variantName: "Róża"
+            },
+            {
+                id: "1234",
+                variantName: "Bez"
+            },
+        ]
+    },
+    {
+        id: "1234",
+        name: "Anwen - Spanwen - Kula do Kąpieli o Zapachu Bez - 100g",
+        desc: "Kula do kąpieli o zapachu Bez",
+        price: 28.30,
+        available: 12,
+        poducerId: "123",
+        producerName: "SkinTra",
+        variants: [
+            {
+                id: "1231",
+                variantName: "Magnolia"
+            },
+            {
+                id: "1232",
+                variantName: "Róża"
+            },
+            {
+                id: "1234",
+                variantName: "Bez"
+            },
+        ]
+    }
+]
+export default defineEventHandler((event) => {
+    return products.find((product)=> product.id === `${event.context.params!.id}`)
+})
