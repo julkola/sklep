4ab8e8b69d3d4045fda3aaac95cfc0d0c5e1cf6a
Ju <julia.kolano66@gmail.com>
2023-05-06 14:41:15 +0200

    category tree start

--- nuxt-app/pages/products/index.vue
+++ nuxt-app/pages/products/index.vue
@@ -1,17 +1,31 @@
 <template>
-    <div id="filters"></div>
-    <div class="flex flex-wrap px-4 py-4">
-        <div v-for="product in products" class=" mr-4 mb-4 w-52 flex flex-col">
-            <div class="w-52 h-52 relative">
-                <img :src="`/products/${product.id}.jpg`" :alt="product.name" class="w-52 h-52 bg-gray-300 rounded ">
-                <button @click="cart.addToCart(product.id,1,product.price,product.available)" class="rounded-full bg-gray-900 shadow text-white w-8 h-8 flex justify-center items-center absolute bottom-3 right-3">
-                    <BasketIcon/>
-                </button>
+    <div class="flex px-4 py-4 ">
+        <div class="w-60 mr-4 px-6 py-4 rounded shadow-lg">
+            <div id="category-tree">
+                <span class="font-semibold">{{ currentCategory.name }}</span>
+                <div class="flex flex-col">
+                    <NuxtLink v-for="subCategory in currentCategory.subCategories" :to="`/category/${subCategory.id}`">{{ subCategory.name }}</NuxtLink>
+                </div>
             </div>
-            <NuxtLink :to="`/products/${product.id}`" class="text-sm">{{ product.name }}</NuxtLink>
-            <p class="text-right mt-2 font-semibold flex-1 flex items-end justify-end">
-                {{ product.price.toFixed(2) }}zł
-            </p>
+            <div id="filters"></div>
+        </div>
+        <div class="flex-1 ">
+            <h1 class="font-bold text-xl mb-4">{{ currentCategory.name }}</h1>
+            <p class="mb-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum sapiente aut, fugit rerum rem atque pariatur deleniti quibusdam porro voluptate libero quod inventore corrupti eos illum suscipit voluptatum eveniet impedit!</p>
+            <div class="flex flex-wrap">
+                <div v-for="product in products" class=" mr-4 mb-4 w-52 flex flex-col">
+                    <div class="w-52 h-52 relative">
+                        <img :src="`/products/${product.id}.jpg`" :alt="product.name" class="w-52 h-52 bg-gray-300 rounded ">
+                        <button @click="cart.addToCart(product.id,1,product.price,product.available)" class="rounded-full bg-gray-900 shadow text-white w-8 h-8 flex justify-center items-center absolute bottom-3 right-3">
+                            <BasketIcon/>
+                        </button>
+                    </div>
+                    <NuxtLink :to="`/products/${product.id}`" class="text-sm">{{ product.name }}</NuxtLink>
+                    <p class="text-right mt-2 font-semibold flex-1 flex items-end justify-end">
+                        {{ product.price.toFixed(2) }}zł
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -19,5 +33,20 @@
 import { useCartStore } from '~~/stores/cart';
 
 const cart = useCartStore();
-const products = useFetch(`/api/product`).data
+const products = useFetch(`/api/product`).data;
+
+const currentCategory = {
+    id: 52432,
+    name: "Do kąpieli",
+    subCategories: [
+        {
+            id: 43430,
+            name: "Kule do kąpieli",
+        },
+        {
+            id: 43431,
+            name: "Sole do kąpieli",
+        }
+    ]
+}
 </script>
