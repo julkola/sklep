ec781cb3b9995163d1ebe2d529bbb326dccaea04
Ju <julia.kolano66@gmail.com>
2023-07-02 19:27:26 +0200

    checkbox_first_styles

--- nuxt-app/components/Checkbox.vue
+++ nuxt-app/components/Checkbox.vue
@@ -1,15 +1,15 @@
 <template>
-    <label :for="props.id" class="flex">
-        <div class="bg-yellow-700 w-4 h-4">
-            a
-        </div>
+    <div class="">
         <input
             :id="props.id"
             type="checkbox"
             v-model="value"
+            class="absolute scale-0 peer"
         />
-        <span>{{ props.text }}</span>
-    </label>
+        <label :for="props.id" class="relative flex cursor-pointer select-none pl-6 after:absolute after:w-4 after:h-4 after:my-1 after:mr-2 after:top-0 after:left-0 after:rounded-sm after:border-gray-900 after:border peer-checked:after:bg-gray-300">
+            <span>{{ props.text }}</span>
+        </label>
+    </div>
 </template>
 <script setup lang="ts">
     const props = defineProps(["id", "text", 'modelValue']);
